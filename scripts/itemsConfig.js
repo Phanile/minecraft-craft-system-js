cd8ec39478f7bf97100fc8b85fa72104f1bb0136
Phanile <pasha_vasilev_2003@mail.ru>
2021-02-25 20:46:06 +0300

    fix inventory + add new items + start craft logic

--- scripts/itemsConfig.js
+++ scripts/itemsConfig.js
@@ -2,18 +2,22 @@ const items = [
     {
         'id' : 1,
         'name' : 'stick',
+        'url' : 'images/stick.png',
         'isCraftable' : false
     },
     {
         'id' : 2,
         'name' : 'diamond',
+        'url' : 'images/diamond.png',
         'isCraftable' : false
     },
     {
         'id' : 3,
         'name' : 'diamond pick',
+        'url' : 'images/pick.png',
         'isCraftable' : true,
-        'craft-items-config' : [
+        'isHomogeneousCraftable' : false,
+        'craftItemsConfig' : [
             {
                 'id' : 1,
                 'pos' : [[5, 8]]
@@ -23,10 +27,41 @@ const items = [
                 'pos' : [[1, 2, 3]]
             }
         ]
+    },
+    {
+        'id' : 4,
+        'name' : 'diamond boots',
+        'url' : 'images/diamondBoots.png',
+        'isCraftable' : true,
+        'isHomogeneousCraftable' : true,
+        'craftItemsConfig' : [
+            {
+                'id' : 2,
+                'pos' : [[1, 3, 4, 6]] //, [4, 6, 7, 9]
+            }
+        ]
+    },
+    {
+        'id' : 5,
+        'name' : 'diamond helmet',
+        'url' : 'images/diamondHelmet.png',
+        'isCraftable' : true,
+        'isHomogeneousCraftable' : true,
+        'craftItemsConfig' : [
+            {
+                'id' : 2,
+                'pos' : [[1, 2, 3, 4, 6]] //, [4, 6, 7, 9]
+            }
+        ]
     }
 ]
 
-for (item of items)
-{
-    console.log(item)
+const createItem = (id) => {
+    let item = items[id - 1]
+    let div = document.createElement('div')
+    div.className = 'item'
+    div.setAttribute('data-ItemId', `${item.id}`)
+    div.setAttribute('draggable', 'true')
+    div.style.backgroundImage = `url('${item.url}')`
+    return div
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,137 @@
 document.addEventListener("DOMContentLoaded", () => {
     const cards = document.getElementsByClassName('item')
     const cells = document.getElementsByClassName('cell')
-    const items = document.getElementsByClassName('items')[0]
+    const craftCells = document.getElementsByClassName('craft')
+    const resultCell = document.getElementsByClassName('resultCraft')[0]
 
     let currentCard = null
-    let filler = document.createElement('div');
-    filler.classList.add('item')
+    
+    const changeCellProperty = (cell) => {
+        if (cell.classList.contains('empty')) {
+            cell.classList.remove('empty')
+            cell.classList.add('full')
+            return
+        }
+        if (cell.classList.contains('full')) {
+            cell.classList.remove('full')
+            cell.classList.add('empty')
+        }
+    }
 
-    const dragStart = function()
-    {
+    const canDropItemOnCell = (cell) => {
+        if (cell.classList.contains('empty') && !cell.classList.contains('resultCraft')) return true
+        else return false
+    }
+
+    const changeItemIdOnCell = (cell, item) => {
+        cell.dataset.itemid = item.dataset.itemid
+    }
+
+    const clearCellItemId = (cell) => {
+        cell.dataset.itemid = 0
+    }
+
+    const findCraft = () => {
+        for (item of items) {
+            if (item.isCraftable) {
+                if (item.isHomogeneousCraftable) {
+                    let x = item.craftItemsConfig[0]
+                    let itemId = x.id
+                    if (x.pos.length == 1) {
+                        if (compareArrayWithWorkbench(x.pos[0], itemId, item, x.pos[0].length) != null) {
+                            console.log('FIND', item.name, item.id)
+                            createResultCraftItem(item.id)
+                        }
+                    }
+                    else {
+                        for (i of x.pos) {
+                            console.log(i)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const compareArrayWithWorkbench = (array, itemId, craftItem, countOfItems) => {
+        let compareCount = 0
+        for (cell of craftCells) {
+            if (!canDropItemOnCell(cell) && cell.classList.contains('craft')) {
+                if (cell.dataset.itemid == itemId) {
+                    for (let i = 0; i < array.length; i++) {
+                        if (cell.dataset.id == array[i]) {
+                            compareCount += 1
+                        }
+                    }
+                }
+            }
+        }
+        if (compareCount == array.length && compareCount == countOfItems) {
+            return craftItem
+        }
+        return null
+    }
+
+    const createResultCraftItem = (id) => {
+        item = createItem(id)
+        AddListenersToCard(item)
+        resultCell.append(item)
+        changeCellProperty(resultCell)
+    }
+
+    const clearWorkbench = () => {
+        for (cell of craftCells) {
+            cell.innerHTML = ``
+            if (cell.classList.contains('full')) {
+                cell.classList.remove('full')
+                cell.classList.add('empty')
+            }
+        }
+        resultCell.classList.remove('full')
+        resultCell.classList.add('empty')
+    }
+
+    const dragDrop = function() {
+        if (!canDropItemOnCell(this)) return
+
+        let lastCell = currentCard.parentNode
+        if (lastCell.classList.contains('resultCraft')) {
+            clearWorkbench()
+        }  
+        changeCellProperty(lastCell)
+        changeCellProperty(this)
+        clearCellItemId(lastCell)
+        this.append(currentCard)
+        changeItemIdOnCell(this, currentCard)
+        findCraft()
+    }
+
+    const dragStart = function() {
         setTimeout(() => {
             this.classList.add('hide')
             currentCard = this
+            console.log(currentCard)
         })
     }
 
-    const dragEnd = function()
-    {
+    const dragEnd = function() {
         this.classList.remove('hide')
     }
 
-    const dragOver = function(event)
-    {
+    const dragOver = function(event) {
         event.preventDefault()
     }
 
-    const dragDrop = function()
-    {
-        this.append(currentCard)
-        items.appendChild(filler)
-    }
-
-    for (var card of cards)
-    {
+    const AddListenersToCard = (card) => {
         card.addEventListener('dragstart', dragStart)
         card.addEventListener('dragend', dragEnd)
-        
     }
 
-    for (var cell of cells)
-    {
+    for (var card of cards) {
+        AddListenersToCard(card)
+    }
+
+    for (var cell of cells) {
         cell.addEventListener('dragover', dragOver)
         cell.addEventListener('drop', dragDrop)
     }
